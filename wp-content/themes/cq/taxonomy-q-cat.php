2fe2afe3be8507f9d481de5a83b96084e382ed49
Wojtek Pilich <chilip@o2.pl>
2018-06-14 21:19:40 +0200

    created q-cat taxonomy

---
+++ wp-content/themes/cq/taxonomy-q-cat.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+
+    <div class="q-cat-container">
+
+
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <!-- post -->
+        <section class="q-cat-question">
+            <h1><?php the_title(); ?></h1>
+            <a href="<?php the_permalink(); ?>" class="">Rozwiąż quiz!</a>
+
+
+            <?php
+            if( have_rows('questions') ):
+                // loop through the rows of data
+                while ( have_rows('questions') ) : the_row();
+                    $question = get_sub_field('question');
+
+                ?>
+<!--                <p>--><?php //echo $question ?><!--</p>-->
+
+
+
+
+                <?php endwhile;
+            else:
+            endif;
+        ?>
+
+        </section>
+
+
+    <?php endwhile; ?>
+      <!-- post navigation -->
+    <?php else: ?>
+      <!-- no posts found -->
+    <?php endif; ?>
+
+    </div>
+<?php get_footer(); ?>
