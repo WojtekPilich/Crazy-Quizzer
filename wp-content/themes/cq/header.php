6c1040d68db90ef01ebb1b9625735b79f104633f
Wojtek Pilich <chilip@o2.pl>
2018-06-23 20:01:26 +0200

    rebuilding header

--- wp-content/themes/cq/header.php
+++ wp-content/themes/cq/header.php
@@ -8,14 +8,18 @@
     <?php wp_head(); ?>
 </head>
     <body>
-        <header class="header">
-            <nav class="nav">
-                <a href="<?php echo site_url('/') ?>" class="nav__logo"><img src="<?php echo get_bloginfo('template_directory'); ?>/img/questions-3409194_640.png" alt="question mark"></a>
-                <p class="nav__title">Witaj w Crazy Quizzer!</p>
-                    <?php wp_nav_menu([
-                            'menu' => 'main',
-                            'menu_class' => 'nav__menu'
-                    ]); ?>
-
-            </nav>
-        </header>
+        <div class="dim">
+            <div class="main-cnt">
+                <header class="header">
+                    <nav class="nav">
+                        <a href="<?php echo site_url('/') ?>" class="nav__logo"><img src="<?php echo get_bloginfo('template_directory'); ?>/img/question_mark_PNG96.png" alt="question mark"></a>
+                        <p class="nav__title">Witaj w Crazy Quizzer!</p>
+                            <?php wp_nav_menu([
+                                    'menu' => 'main',
+                                    'menu_class' => 'nav__menu'
+                                ]); ?>
+                        <div class="login-register-btn">
+                            <a href="#"><img src="<?php echo get_bloginfo('template_directory'); ?>/img/user_access-512.png" alt=""></a>
+                        </div>
+                    </nav>
+                </header>
