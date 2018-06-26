2faf2158ea42a7293c7af1c123bdfacfc30e4e40
Wojtek Pilich <chilip@o2.pl>
2018-06-26 22:20:10 +0200

    developing scripts

--- wp-content/themes/cq/js/app.js
+++ wp-content/themes/cq/js/app.js
@@ -1,40 +1,63 @@
 (function($) {
 
+    //login-form
     const loginRegisterForm = $('.login-register');
     const loginRegisterBtn = $('.login-register-btn');
+    const dim = $('.dim');
 
-    loginRegisterForm.addClass('hidden');
+    dim.hide();
+    loginRegisterForm.hide();
 
     loginRegisterBtn.on('click', function(e) {
         // e.preventDefault();
-        loginRegisterForm.toggleClass('showed');
+        loginRegisterForm.show();
+        dim.show();
     });
 
+    dim.on('click', function() {
+       $(this).hide();
+       loginRegisterForm.hide();
+    });
+
+    //slider
     let slider = $('.main-slider');
     slider.slick({
-        speed: 900,
+        speed: 1000,
         arrows: true,
         dots: true,
         slidesToShow: 1,
         infinite: true,
-        centerMode: true,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        mobileFirst: true,
+        // centerMode: true,
+        // autoplay: true,
+        // autoplaySpeed: 1500,
+        // mobileFirst: true,
         touchMove: true,
-        // variableWidth: true
+        variableWidth: true,
+        // adaptiveHeight: true,
         // pauseOnFocus: true
         // fade: true,
         // cssEase: 'linear',
         // rtl: true
         // autoplay: true
+        // centerPadding: '20px'
     });
+
+    let sliderTit = $('.slider-title');
+    let sliderImg = $('.slider-img');
+
+    sliderTit.hide();
+
+    sliderImg.on('mouseenter', function(e) {
+        $(this).prev().show();
+    });
+    sliderImg.stopImmediatePropagation();
+
+    sliderImg.on('mouseleave', function(e) {
+        $(this).prev().hide();
+    });
+
+
 
 })(jQuery);
 
-// (function($) {
-//
-//     // alert('ok');
-//     $('.main-slider').slick();
-//
-// })(jQuery);
+
