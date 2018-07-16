58dd1b4a384e92c75ca8248bccdb5c8bd67e9375
Wojtek Pilich <chilip@tlen.pl>
2018-07-16 18:37:58 +0200

    little 404 page update

--- wp-content/themes/cq/404.php
+++ wp-content/themes/cq/404.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
     <section class="error-404-msg">
-        <h1>Oops! It seems to be a wrong place...</h1>
-        <p>Better <a href="<?= site_url('/')?>">come back</a> and enjoy more crazy quizzes :)</p>
+        <h1>Ups! To nie jest właściwy adres...</h1>
+        <p>Lepiej <a href="<?= site_url('/')?>">wracaj</a> i rozwiązuj kolejne quizy :)</p>
     </section>
 
     <?php get_template_part('login-form') ?>
